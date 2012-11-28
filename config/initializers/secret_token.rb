24095ec084243808e911ae7aa24b9b8f52b68fab
Grant Rodgers <grantr@gmail.com>
2012-11-27 19:11:42 -0800

    rails new commitment --edge
    Using rails 3.2.9

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Commitment::Application.config.secret_token = '88f99696c2dd3d6a154aba78204a312d379a9c5b95eca4b87d6f710017eea060e6454de8c85033b417d699e7c59cde8c294965f4d732170eca4cf331fe6d3215'
