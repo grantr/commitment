a9fd722c9f41c78fab3818369070f9486eb588c5
Grant Rodgers <grantr@gmail.com>
2012-11-28 10:13:48 -0800

    rails new commitment --skip-active-record --skip-javascript --skip-index-html --edge
    Using rails master (3e965e2)

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -2,6 +2,11 @@
 
 # Your secret key for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Commitment::Application.config.secret_token = '88f99696c2dd3d6a154aba78204a312d379a9c5b95eca4b87d6f710017eea060e6454de8c85033b417d699e7c59cde8c294965f4d732170eca4cf331fe6d3215'
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Commitment::Application.config.secret_key_base = '7bd36e9f5209dd585430ac786e222c13f5b264196c0ad3301902a58add4e4bbc14a91cf2e9e9f071ef40cc623983dd73628e71ade2ab209baaebcd3d2d879e59'
